f294bc8bb8c442b9e0e054e54eb8b7967f1bfa21
HugoRr5 <132697347+HugoRr5@users.noreply.github.com>
2023-08-11 13:35:14 +0000

    atualizaçao

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
 
 import express from "express";      // Requisição do pacote do express
 const app = express();              // Instancia o Express
-const port = 3000;                  // Define a porta
+const port = 3000;  
+
+app.use(express.json());
+                // Define a porta
 
 app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
   res.json({
@@ -16,6 +19,7 @@ app.listen(port, () => {            // Um socket para "escutar" as requisições
   console.log(`Serviço escutando na porta:  ${port}`);
 });
 
+//index.js
 //index.js
 import dotenv from "dotenv";
 dotenv.config();
@@ -50,8 +54,7 @@ app.get("/usuario/:id", async (req, res) => {
 
 
 
-//index.js
-app.use(express.json());
+
 
 
 //index.js
