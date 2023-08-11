e95d2314641aebc5f7c9e2803f5bdf7099e71714
HugoRr5 <132697347+HugoRr5@users.noreply.github.com>
2023-08-11 13:27:04 +0000

    atualizaçao

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ app.listen(port, () => {            // Um socket para "escutar" as requisições
 import dotenv from "dotenv";
 dotenv.config();
 
-import {  selectUsuarios, selectUsuario} from "./bd.js";
+
+//index.js
+import { selectUsuarios, selectUsuario, insertUsuario } from "./bd.js";
 
 
 app.get("/usuarios", async (req, res) => {
@@ -41,6 +43,23 @@ app.get("/usuario/:id", async (req, res) => {
     const usuario = await selectUsuario(req.params.id);
     if (usuario.length > 0) res.json(usuario);
     else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
+
+
+//index.js
+app.use(express.json());
+
+
+//index.js
+app.post("/usuario", async (req, res) => {
+  console.log("Rota POST /usuario solicitada");
+  try {
+    await insertUsuario(req.body);
+    res.status(201).json({ message: "Usuário inserido com sucesso!" });
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
