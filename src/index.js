51bd4c64adca9c19a6584554398d90be06147ee7
HugoRr5 <132697347+HugoRr5@users.noreply.github.com>
2023-08-25 17:14:44 +0000

    atualizaçao

---
+++ src/index.js
@@ -0,0 +1,31 @@
+
+import express from "express";      // Requisição do pacote do express
+import dotenv from "dotenv";
+import roteadorUsuario from "./routes/usuario.js";
+//index.js
+dotenv.config();
+
+
+const app = express();//se de error tira isso 
+// Instancia o Express
+const port = 3000;  
+
+
+
+
+app.use(express.json());
+// Define a porta
+app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
+  res.json({
+    nome: "hugo rodrigues rocha ",      // Substitua pelo seu nome
+  });
+  console.log("Rota / solicitada");
+});
+
+app.listen(port, () => {            // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
+
+//estava a linha 8
+
+app.use(roteadorUsuario);
