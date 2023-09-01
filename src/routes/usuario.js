ea8047e951c1e9eef6e291e32f8673c6db55c98a
HugoRr5 <132697347+HugoRr5@users.noreply.github.com>
2023-09-01 20:54:27 +0000

    atualizaçao

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -11,7 +11,7 @@ import {
 
 const router = Router();
 
-router.get("/usuario",verificarAutenticacao, async (req, res) => {
+router.get("/usuario", async (req, res) => {
   console.log("Rota GET /usuario solicitada");
   try {
     const usuarios = await selectUsuarios();
@@ -21,7 +21,7 @@ router.get("/usuario",verificarAutenticacao, async (req, res) => {
   }
 });
 
-router.get("/usuario/:id",verificarAutenticacao, async (req, res) => {
+router.get("/usuario/:id", async (req, res) => {
   console.log(`Rota GET /usuario/${req.params.id} solicitada`);
   try {
     const usuario = await selectUsuario(req.params.id);
